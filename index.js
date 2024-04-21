e64946024f3c9d8ea8beac95d6d3366bdd18f86e
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-04-21 23:08:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -65,3 +65,35 @@ const gridGenerator = () => {
     }
   }
 };
+//Click the image
+const selectImage = (e) => {
+  e.preventDefault();
+  //Set currentElement
+  currentElement = e.target;
+  //target(blank image)
+  let targetElement = document.querySelector(".target");
+  let currentParent = currentElement.parentElement;
+  let targetParent = targetElement.parentElement;
+  //get row and col values for both elements
+  const [row1, col1] = getCoords(currentParent);
+  const [row2, col2] = getCoords(targetParent);
+  if (checkAdjacent(row1, row2, col1, col2)) {
+    //Swap
+    currentElement.remove();
+    targetElement.remove();
+    //Get image index(to be used later for manipulating array)
+    let currentIndex = parseInt(currentElement.getAttribute("data-index"));
+    let targetIndex = parseInt(targetElement.getAttribute("data-index"));
+    //Swap Index
+    currentElement.setAttribute("data-index", targetIndex);
+    targetElement.setAttribute("data-index", currentIndex);
+    //Swap Images
+    currentParent.appendChild(targetElement);
+    targetParent.appendChild(currentElement);
+    //Array swaps
+    let currentArrIndex = imagesArr.indexOf(currentIndex);
+    let targetArrIndex = imagesArr.indexOf(targetIndex);
+    [imagesArr[currentArrIndex], imagesArr[targetArrIndex]] = [
+      imagesArr[targetArrIndex],
+      imagesArr[currentArrIndex],
+    ];
